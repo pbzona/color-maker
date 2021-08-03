0ec83be2b088333e27e67abfbb41545d39721d50
Phil Zona <pzona@launchdarkly.com>
2021-08-03 17:20:38 -0500

    Autoformat

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,62 @@
 import { useState } from 'react';
 import store from 'store';
-import md5 from 'md5'
+import md5 from 'md5';
 import { useFlags, useLDClient } from 'launchdarkly-react-client-sdk';
 
 import styles from '../css/Header.module.css';
 
 function Header() {
-  const [username, setUsername] = useState('Sample User')
-  const { loginEnabled, redHeaderBackground } = useFlags()
-  const ldClient = useLDClient()
-  
-  function simulateLogin(event) {
-    event.preventDefault()
-    if (username) {
-      const name = username
-      const email = `${username}@hotmail.gov`
+  const [username, setUsername] = useState('Sample User');
+  const { loginEnabled, redHeaderBackground } = useFlags();
+  const ldClient = useLDClient();
 
-      store.set('name', username)
-      store.set('key', md5(username))
+  function simulateLogin(event) {
+    event.preventDefault();
+    if (username) {
+      const name = username;
+      const email = `${username}@hotmail.gov`;
+
+      store.set('name', username);
+      store.set('key', md5(username));
 
       let userObject = {
-        name, 
+        name,
         key: md5(username),
-        email
-      }
+        email,
+      };
 
-      ldClient.identify(userObject , null, function() {
-        console.log(`Now logged in as: ${username}`)
-      })
+      ldClient.identify(userObject, null, function () {
+        console.log(`Now logged in as: ${username}`);
+      });
     }
   }
 
   function handleInputChange(event) {
-    setUsername(event.target.value)
+    setUsername(event.target.value);
   }
 
   return (
-    <header className={`${styles.header} ${redHeaderBackground && styles.headerAlt}`}>
+    <header
+      className={`${styles.header} ${redHeaderBackground && styles.headerAlt}`}
+    >
       <h1 className={styles.headerText}>ColorMaker</h1>
-      { loginEnabled && 
-        (
-          <form onSubmit={simulateLogin}>
-            <label className={styles.label} htmlFor="user">Username: </label>
-            <input
-              className={styles.input}
-              type="text"
-              name="user"
-              value={username}
-              onChange={handleInputChange}
-            />
-            <input className={styles.btn} type="submit" value="Log in"/> 
-          </form>
-        )
-      }
+      {loginEnabled && (
+        <form onSubmit={simulateLogin}>
+          <label className={styles.label} htmlFor="user">
+            Username:{' '}
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            name="user"
+            value={username}
+            onChange={handleInputChange}
+          />
+          <input className={styles.btn} type="submit" value="Log in" />
+        </form>
+      )}
     </header>
-  )
+  );
 }
 
 export default Header;
